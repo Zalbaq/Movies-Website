734e4739b6d705b1d5adb860d0016b76f1ceb1c5
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-30 15:30:25 +0700

    [create] card components

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import "../css/tailwind.css";
+import "../css/Card.css";
+
+const Card = ({ title, imgUrl }) => {
+  return (
+    <>
+      <div className="card relative h-[30rem] w-[15rem] p-2">
+        <img
+          className="h-[22rem] rounded-2xl object-contain"
+          src={`https://image.tmdb.org/t/p/w300${imgUrl}`}
+        />
+        <h3 className="flex flex-col justify-center items-center mt-3 text-white text-2xl text-center whitespace-normal">
+          {title}
+        </h3>
+      </div>
+    </>
+  );
+};
+export default Card;
