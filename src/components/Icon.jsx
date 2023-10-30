b59bbc3cc75fdc7e0c1ae26e14dc48f56132b71f
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-30 15:30:37 +0700

    [create] icon components

---
+++ src/components/Icon.jsx
@@ -0,0 +1,11 @@
+const Icon = () => {
+    return(
+        <>
+        <div className="icon">
+            <h1 className="text-[2rem] tracking-[2px] font-bold">MovieZa</h1>
+        </div>
+        </>
+    )
+}
+
+export default Icon;
