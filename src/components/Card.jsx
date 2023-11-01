0a4e18eb4e5e0872375cfa11b20b63f5733ff114
Zalbaq <ihza.nk.160903@gmail.com>
2023-11-01 22:41:05 +0700

    [remake:basis] style card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,9 @@ import "../css/tailwind.css";
 const Card = ({ title, imgUrl }) => {
   return (
     <>
-      <div className="card relative h-[30rem] w-[15rem] p-2">
+      <div className="card basis-[20rem] relative h-[30rem] w-[15rem] p-5 mb-10">
         <img
-          className="h-[22rem] rounded-2xl object-contain"
+          className=" rounded-2xl object-contain"
           src={`https://image.tmdb.org/t/p/w300${imgUrl}`}
         />
         <h3 className="flex flex-col justify-center items-center mt-3 text-white text-2xl text-center whitespace-normal">
