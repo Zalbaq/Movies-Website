03da01f19853b38200f03641fba145fa67a9fd1e
Zalbaq <ihza.nk.160903@gmail.com>
2023-11-01 22:38:48 +0700

    [add] pages Top rate movies

---
+++ src/pages/TopRatesPage.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { getTvList } from "../api/api";
+import styled from "styled-components";
+import Card from "../components/Card";
+import { useLocation } from "react-router-dom";
+
+const TvPage = ({ movieTitle }) => {
+  const [tv, setTv] = useState([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (movieTitle.length < 4 && location.pathname === "/tv") {
+      getTvList().then((result) => {
+        setTv(result);
+      });
+    }
+  }, [movieTitle]);
+
+  return (
+    <>
+      <Section className="relative flex flex-wrap justify-center items-center gap-1 py-[2rem] px-[3rem]">
+        {tv.map((movie, i) => (
+          <Card
+            key={i}
+            title={movie.original_title}
+            imgUrl={movie.poster_path}
+          />
+        ))}
+      </Section>
+    </>
+  );
+};
+const Section = styled.section`
+  margin-top: 6rem;
+  background: #222831;
+`;
+
+export default TvPage;
