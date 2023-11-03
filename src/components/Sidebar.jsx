10dcf50e58e7c7ec1a55c9e5e12635209c2a755e
Zalbaq <ihza.nk.160903@gmail.com>
2023-11-03 23:58:00 +0700

    [create] Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import { Squash as Hamburger } from "hamburger-react";
 import ListMenu from "./ListMenu";
 
-const Sidebar = () => {
-  const [isOpen, setOpen] = useState(false);
-
+const Sidebar = ({ className }) => {
   return (
     <>
-      <div className="absolute h-[100vh] w-[25rem] top-0 left-0 flex flex-col bg-red-400">
-        <Hamburger />
-        <ListMenu />
+      <div className={className}>
+        <ListMenu className={"flex flex-col text-[2rem] gap-[5rem] text-white"}/>
       </div>
     </>
   );
