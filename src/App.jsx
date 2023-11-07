caa8f4a065f60b75a90f26994b79a924f6e702fe
Zalbaq <ihza.nk.160903@gmail.com>
2023-11-07 20:14:44 +0700

    [change] path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./css/tailwind.css";
 import "./css/App.css";
 import Popular from "./pages/Popular";
-import TvPage from "./pages/TopRatesPage";
-import Navbar from "./components/Navbar";
+import TopRatesPage from "./pages/TopRatesPage";
+import Navbar from "./layouts/Navbar";
 import SearchPage from "./pages/SearchPage";
 
 function App() {
@@ -26,12 +26,12 @@ function App() {
             }
           />
           <Route
-            path="/tv"
+            path="/topRated"
             element={
               searchMovie.length > 3 ? (
                 <SearchPage movieTitle={searchMovie} />
               ) : (
-                <TvPage movieTitle={searchMovie} />
+                <TopRatesPage movieTitle={searchMovie} />
               )
             }
           />
